5d153e0a67d2d396207cb030d26be208f9bf1282
jqyssi <jqyssi2018@gmail.com>
2023-07-06 00:13:43 +0800

    Revert "第二次修改"
    This reverts commit 63db9d33f3d9af9f824b416e0aaac0f295a8abaf.

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -4,6 +4,5 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("第一次修改！");
-        System.out.println("第二次修改！");
     }
 }
