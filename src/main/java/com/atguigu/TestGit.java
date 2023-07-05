9b8d9b54065d038513541a4f42d6b0753dea0f8b
jqyssi <jqyssi2018@gmail.com>
2023-07-06 00:09:42 +0800

    Revert "第一次修改"
    This reverts commit 3b92af5d

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -3,8 +3,5 @@ package com.atguigu;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("第一次修改！");
-        System.out.println("第二次修改！");
-        System.out.println("第3次修改！");
     }
 }
