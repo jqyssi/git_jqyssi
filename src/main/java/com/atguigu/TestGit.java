3dd645290082246617189925ecfd4387ed264ba8
jqyssi <jqyssi2018@gmail.com>
2023-07-06 00:10:17 +0800

    Revert "第3次修改"
    This reverts commit e99125d6

--- src/main/java/com/atguigu/TestGit.java
+++ src/main/java/com/atguigu/TestGit.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("第一次修改！");
+        System.out.println("第二次修改！");
     }
 }
